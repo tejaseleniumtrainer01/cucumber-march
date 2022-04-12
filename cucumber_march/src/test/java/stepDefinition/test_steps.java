074810b87b561009a2218636b45dc771e3aa1d7e
tejas <tejas@DESKTOP-BD2AFT7.localdomain>
2022-04-12 06:23:26 -0400

    test_step feature file was updated

--- cucumber_march/src/test/java/stepDefinition/test_steps.java
+++ cucumber_march/src/test/java/stepDefinition/test_steps.java
@@ -25,6 +25,7 @@ public class test_steps {
 	public String browserType;
 	public final String screenshotsFolderPath = System.getProperty("user.dir") + "\\output\\screenshots\\";
 	public static WebDriver driver;
+	
 	@Given("^User is on Home Page$")
 	public void user_is_on_Home_Page() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
