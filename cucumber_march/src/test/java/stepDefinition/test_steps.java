6d15e080b65db64c23a5bb45f14dc5b1795fd2bc
tejas <tejas@DESKTOP-BD2AFT7.localdomain>
2024-10-03 22:46:42 -0400

    code udpated

--- cucumber_march/src/test/java/stepDefinition/test_steps.java
+++ cucumber_march/src/test/java/stepDefinition/test_steps.java
@@ -33,7 +33,7 @@ public class test_steps {
 	public void user_is_on_Home_Page() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		ChromeOptions co=new ChromeOptions();
-		co.setBrowserVersion("122");
+		co.setBrowserVersion("129");
 		driver=new ChromeDriver(co);
 		driver.manage().window().maximize();
 //		driver.get("https://opensource-demo.orangehrmlive.com/");
